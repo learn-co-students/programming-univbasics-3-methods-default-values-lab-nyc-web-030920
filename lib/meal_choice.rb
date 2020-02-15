86efe9f1428b67c9b3632d2a862a6a08b5dc3b3f
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-15 04:56:11 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,11 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(veg_one, veg_two, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg_one} and #{veg_two}."
+end 
+
+meal_choice("broccoli", "macaroni", "tofu") 
+
+#2020.02.14.JP: Question: How do I give this method an explicit return value? (so that the return value includes the three food items I've chosen?) 
